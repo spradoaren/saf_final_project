74f223c7ebfb07a9d72a4f395024464db1597b6a
graceyao0826 <ry2529@columbia.edu>
2026-04-05 14:36:24 -0400

    Delete evaluation.py

--- evaluation.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def mse(y_true, y_pred):
-    return np.mean((y_true - y_pred) ** 2)
-
-
-def directional_accuracy(y_true, y_pred):
-    return np.mean(np.sign(y_true) == np.sign(y_pred))
